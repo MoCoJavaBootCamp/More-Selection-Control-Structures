a7ed0246e52fa9ad8aea95619a5ab3ab38c6d4e2
azdoolittle <drewdoolittle@gmail.com>
2020-04-20 16:37:49 -0400

    pseudocode

--- src/CustomerRecord.java
+++ src/CustomerRecord.java
@@ -1,2 +1,24 @@
+/*
+A program is required to read a customer’s name, a purchase amount and a tax code.
+        The tax code has been validated and will be one of the following:
+        0  tax exempt (0%)
+        1  state sales tax only (3%)
+        2  federal and state sales tax (5%)
+        3  special sales tax (7%)
+        The program must then compute the sales tax and
+            the total amount due, and print the customer’s name,
+            purchase amount, sales tax and total amount due.
+
+Pseudocode:
+import Scanner
+declare method
+declare vars for scanner, 0 tax, state, fed and state, special tax,
+    applicable tax, total due, customer name
+print customer's name
+    purchase amount
+    sales tax
+    total amount
+ */
 public class CustomerRecord {
+
 }
